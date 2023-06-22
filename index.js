12c7aec7ee8f883d1a3de5f6e8e36508d16846b3
jacobgonzales <jagonz@live.com>
2023-06-22 15:42:43 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
 /* Your Code Here */
 
+function createEmployeeRecord([strFirstName, strFamilyName, strTitle, numPayPerHour]) {
+    let obj = {
+        firstName : strFirstName,
+        familyName : strFamilyName,
+        title : strTitle,
+        payPerHour : numPayPerHour,
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return obj
+};
+
+function createEmployeeRecords(employeeRecords) {
+    let records = []
+    employeeRecords.forEach(element => {
+        records.push(createEmployeeRecord(element))
+    });
+    return records 
+};
+
+function createTimeInEvent(dateStamp = "2023-06-21 0000") {
+    let obj = {
+        type : 'TimeIn',
+        hour : Number(dateStamp.slice(11)),
+        date : dateStamp.slice(0,10)
+    }
+    this.timeInEvents.push(obj)
+    return this
+};
+
+function createTimeOutEvent(dateStamp = "2023-06-21 0800") {
+    let obj = {
+        type : 'TimeOut',
+        hour : Number(dateStamp.slice(11)),
+        date : dateStamp.slice(0,10)
+    }
+    this.timeOutEvents.push(obj)
+    return this
+};
+
+function hoursWorkedOnDate(date = "YYYY-MM-DD") {
+    for (let i = 0; i < this.timeInEvents.length; i++) {
+        if (this.timeInEvents[i].date === date) {
+            const timeIn = this.timeInEvents[i].hour
+            const timeOut = this.timeOutEvents[i].hour
+            let hoursWorked = (timeOut - timeIn)/100
+            return hoursWorked
+        } else continue
+      }
+};
+
+function wagesEarnedOnDate(date = "YYYY-MM-DD") {
+    let rate = this.payPerHour
+    let hours = hoursWorkedOnDate.call(this, date)
+    let payOwed = rate * hours
+    return payOwed
+};
+/*
+function allWagesFor(record) {
+    let workedDates = []
+    let payOwedForAllDates = 0
+    for (let i = 0; i < record.timeInEvents.length; i++) {
+        workedDates.push(record.timeInEvents[i]['date'])
+    }
+    for (let i = 0; i < workedDates.length; i++) {
+        let date = workedDates[i]
+
+        payOwedForAllDates += (wagesEarnedOnDate(record, date))
+    }
+    return payOwedForAllDates
+};
+*/
+
+function findEmployeeByFirstName(employeeRecords, strFirstName) {
+    return employeeRecords.find(record => record.firstName === strFirstName)
+};
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +100,10 @@ const allWagesFor = function () {
     return payable
 }
 
+function calculatePayroll(employeeRecords) {
+    let payroll = 0
+    employeeRecords.map(employee => {
+        payroll += allWagesFor.call(employee)
+    })
+    return payroll
+};
